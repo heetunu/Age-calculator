334f424b15245ad313aeaa3477a7abc37a09cc94
heetunu <heetunuoluwa@gmail.com>
2024-08-30 23:51:48 +0100

    newCommit

--- age-calculator-app-main/age.js
+++ age-calculator-app-main/age.js
@@ -1,65 +1,36 @@
-const dayInp = document.getElementById("day");
-const monthInp = document.getElementById("month");
-const yearInp = document.getElementById("year");
-
-const dayOtp = document.getElementById("DD");
-const monthOtp = document.getElementById("MM");
-const yearOtp = document.getElementById("YY");
-
-const form = document.querySelector("form");
-
-const date = new Date();
-let day = date.getDate();
-let month = 1 + date.getMonth();
-let year = date.getFullYear();
-
-const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-function validate() {
-  const inputs = document.querySelectorAll("input");
-  let validator = true;
-  inputs.forEach((i) => {
-    const parent = i.parentElement;
-    if (!i.value) {
-      i.style.borderColor = "red";
-      parent.querySelector("small").innerText = "this field is required.";
-      validator = false;
-    } else if (monthInp.value > 12) {
-        monthInp.style.borderColor = "red";
-        monthInp.parentElement.querySelector("small").innerText = "must be valid month.";
-        validator = false;
-    } else if (dayInp.value > 31) {
-        dayInp.style.borderColor = "red";
-        dayInp.parentElement.querySelector("small").innerText =
-          "must be valid day.";
-        validator = false;
-    } else {
-      i.style.borderColor = "black";
-      parent.querySelector("small").innerText = "";
-      validator = true;
-    }
-  });
-  return validator;
-}
-function handleSubmit(e) {
+function calc(e) {
   e.preventDefault();
-  if (validate()) {
-    if (dayInp.value > day) {
-      day = day + months[month - 1];
-      month = month - 1;
-    }
-    if (monthInp.value > month) {
-      month = month + 12;
-      year = year - 1;
-    }
+  const dayInp = document.getElementById("day").value;
+  const monthInp = document.getElementById("month").value;
+  const yearInp = document.getElementById("year").value;
 
-    const d = day - dayInp.value;
-    const m = month - monthInp.value;
-    const y = year - yearInp.value;
+  const dayOtp = document.getElementById("DD");
+  const monthOtp = document.getElementById("MM");
+  const yearOtp = document.getElementById("YY");
 
-    dayOtp.innerHTML = d;
-    monthOtp.innerHTML = m;
-    yearOtp.innerHTML = y;
+  const form = document.querySelector("form");
+
+  const birthDate = new Date(yearInp, monthInp - 1, dayInp);
+  let todayDate = new Date();
+  let year = todayDate.getFullYear() - birthDate.getFullYear();
+  let month = todayDate.getMonth() - birthDate.getMonth()
+  let days = todayDate.getDate() - birthDate.getDate()
+
+  if (month < 0 || (month === 0 && days < 0)) {
+    year--;
+    month += 12;
   }
+
+  if (days < 0) {
+    const prevMonth = new Date(todayDate.getFullYear(), todayDate.getMonth(), 0).getDate();
+    days += prevMonth;
+    month--;
+  }
+
+  dayOtp.innerText = days
+  monthOtp.innerText = month
+  yearOtp.innerText = year
 }
 
-form.addEventListener("submit", handleSubmit);
+document.getElementById('calcForm').addEventListener("submit", calc);
+
